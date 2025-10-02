95fb3e8ef82ef341fd1e8f4414de5594521c390e
Siddartha Nepal <siddarthanepal5@gmail.com>
2025-10-03 00:30:52 +0530

    Add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1334,7 +1334,7 @@
 				<span class="text-base-content/70"
 					>© {new Date().getFullYear()}
 					<span class="font-semibold text-primary">Khadka Foods</span
-					>. All rights reserved. Designed & Developed by
+					>. All rights reserved. </span><span>Designed & Developed by
 					<a
 						href="https://devignity.tech/"
 						class="font-semibold text-primary hover:text-primary/80 transition-colors duration-200"
